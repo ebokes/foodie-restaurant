3f4f1b5e1ca777c6a368b57aad6b62aa7000e0a0
Chibuokem Egbuchulam <talk2chibu@gmail.com>
2025-11-10 13:28:07 +0100

    feat: creater why-use-us section

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import { type Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+          solid: "hsl(var(--primary-solid))",
+        },
+        // ...other colors
+      },
+    },
+  },
+};
+
+export default config;
